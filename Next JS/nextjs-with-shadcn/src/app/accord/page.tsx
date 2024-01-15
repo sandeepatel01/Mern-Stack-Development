cae790d8f0b9ff3d0a5cf9883e6f8fe02b7e0a0e
Sandeep Patel <sandeepatel.0101@gmail.com>
2024-01-16 01:13:21 +0530

    Next JS With Shadcn

---
+++ Next JS/nextjs-with-shadcn/src/app/accord/page.tsx
@@ -0,0 +1,75 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+
+
+
+function AccordPpage() {
+    return (
+        <div className="flex justify-center items-center">
+            <main className="w-[600px] p-6">
+                <Accordion type="single" collapsible>
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+                <Accordion type="single" collapsible>
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+                <Accordion type="single" collapsible>
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </main>
+            <AlertDialog>
+                <AlertDialogTrigger>Open</AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+
+
+        </div>
+
+    )
+}
+
+export default AccordPpage
--- Next JS/nextjs-with-shadcn/src/app/shadcn/page.tsx
+++ Next JS/nextjs-with-shadcn/src/app/shadcn/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button"
 
 function page() {
     return (
-        <div>
-            Shadcn Page 👨‍💻
-        </div>
+        <main className="h-full flex justify-center items-center  flex-col">
+            <div>
+                Shadcn Page 👨‍💻
+            </div>
+            {/* <button className="bg-white text-black py-2 px-6 my-4 rounded-md hover:bg-slate-500">Get Started</button> */}
+            <Button variant="sandy">Get Started</Button>
+        </main>
     )
 }
 
